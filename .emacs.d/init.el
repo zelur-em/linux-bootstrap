e67e4bb1d4d47bd68d016bed0833a8f941dbb0db
bjoern.griebenow@piraten-lsa.de <ze@das-kleine-schwarze>
2013-03-11 00:19:14 +0100

    Added emacs config file.

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(setq-default c-basic-offset 4)
