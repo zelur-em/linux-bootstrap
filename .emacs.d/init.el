9a137cf96addd083b911746fe3d02cba225b9772
Björn Griebenow <b.griebenow@web.de>
2013-03-19 18:33:48 +0100

    blah

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,9 @@
 ;(add-to-list 'load-path ')
 
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("melpa"     . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
@@ -27,16 +27,24 @@
 (require 'ido)
 (ido-mode t)
 (global-set-key (kbd "M-/") 'hippie-expand)
-(setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev 
+					 try-expand-dabbrev-all-buffers 
+					 try-expand-dabbrev-from-kill 
+					 try-complete-file-name-partially 
+					 try-complete-file-name 
+					 try-expand-all-abbrevs 
+					 try-expand-list try-expand-line 
+					 try-complete-lisp-symbol-partially 
+					 try-complete-lisp-symbol))
 
-;(require 'flymake)
-;(global-set-key [f3] 'flymake-display-err-menu-for-current-line)
-;(global-set-key [f4] 'flymake-goto-next-error)
-;(add-hook 'find-file-hook 'flymake-find-file-hook) ; enable flymake for all supported filetypes
-;(setq flymake-gui-warnings-enabled nil) ; allow to show the error without X11 support
+(require 'flymake)
+(global-set-key [f3] 'flymake-display-err-menu-for-current-line)
+(global-set-key [f4] 'flymake-goto-next-error)
+(add-hook 'find-file-hook 'flymake-find-file-hook) ; enable flymake for all supported filetypes
+(setq flymake-gui-warnings-enabled nil)            ; allow to show the error without X11 support
 
-(add-hook 'prog-mode-hook 'flycheck-mode)
-(add-hook 'text-mode-hook 'flycheck-mode)
+;(add-hook 'prog-mode-hook 'flycheck-mode)
+;(add-hook 'text-mode-hook 'flycheck-mode)
 
 (setq c-basic-offset 4)
 
