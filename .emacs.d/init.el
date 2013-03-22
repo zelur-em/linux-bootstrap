889c2786d4024d9235ae5691b2599b4f71c0032d
Björn Griebenow <b.griebenow@web.de>
2013-03-22 11:53:27 +0100

    Emacs: - highlight braces for all prog modes - create backup files on separate directory - added hungry delete mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,8 +24,11 @@
 
 ;(print 'ac-dictionary-directories)
 
+; a lot of ui auto completion
+; where do i get the fuzzy matching ???
 (require 'ido)
 (ido-mode t)
+
 (global-set-key (kbd "M-/") 'hippie-expand)
 (setq hippie-expand-try-functions-list '(try-expand-dabbrev 
 					 try-expand-dabbrev-all-buffers 
@@ -37,13 +40,20 @@
 					 try-complete-lisp-symbol-partially 
 					 try-complete-lisp-symbol))
 
-(require 'flymake)
+(require 'flymake)                                 ; show syntax errors on the fly
 (global-set-key [f3] 'flymake-display-err-menu-for-current-line)
 (global-set-key [f4] 'flymake-goto-next-error)
 (add-hook 'find-file-hook 'flymake-find-file-hook) ; enable flymake for all supported filetypes
-(setq flymake-gui-warnings-enabled nil)            ; allow to show the error without X11 support
+;(setq flymake-gui-warnings-enabled nil)            ; allow to show the error without X11 support
+(require 'flymake-cursor)                          ; show error message in minibuffer
 
-;(add-hook 'prog-mode-hook 'flycheck-mode)
+;highlight the other bracket
+(show-paren-mode)    ; emacs builtin
+;(require 'mic-paren) ; modification of show-paren-mode
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode) ; activate rainbow mode for all programming modes
+
+;(Add-Hook 'prog-mode-hook 'flycheck-mode)
 ;(add-hook 'text-mode-hook 'flycheck-mode)
 
 (setq c-basic-offset 4)
@@ -52,3 +62,14 @@
 (hungry-delete-mode)
 
 (setq linum-format "%4d\u2502")
+
+(setq
+   backup-by-copying t                           ; don't clobber symlinks
+   backup-directory-alist
+   '(("." . "~/local/autogen/emacs_backup"))     ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)                            ; use versioned backups
+
+;(setq make-backup-files nil)                    ; WARNING! dissable all backup files
