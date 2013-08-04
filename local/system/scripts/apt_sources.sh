d2234f614301a914c22c053b3c3822d5ed9a1baf
Björn Griebenow <b.griebenow@web.de>
2013-08-04 03:55:09 +0200

    Added 2 scripts (still very basic and crappy) to manipulate the sources.list on debian

---
+++ local/system/scripts/apt_sources.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Emacs Snapshot"                             >> /etc/apt/sources.list
+echo "deb http://emacs.naquadah.org/ stable/"     >> /etc/apt/sources.list
+echo "deb-src http://emacs.naquadah.org/ stable/" >> /etc/apt/sources.list     
+
